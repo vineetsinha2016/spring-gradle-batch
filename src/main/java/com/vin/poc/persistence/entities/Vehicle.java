826b5acb5fc6c7b5bde218332e8caa6358e2a5dd
vineetsinha2016 <vineet.sinha07@gmail.com>
2022-01-30 17:13:17 -0500

    working code

--- src/main/java/com/vin/poc/persistence/entities/Vehicle.java
+++ src/main/java/com/vin/poc/persistence/entities/Vehicle.java
@@ -1,18 +1,27 @@
 package com.vin.poc.persistence.entities;
 
+import lombok.Data;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity(name = "Vehicle")
+@Data
 public class Vehicle {
     @Id
-    @Column(name = "vehicleId", nullable = false)
+    @Column(name = "vehicleid", nullable = false)
     private String vehicleId;
+    @Column(name = "VIN")
     private String VIN;
+    @Column(name = "model")
     private String model;
+    @Column(name = "year")
     private String year;
+    @Column(name = "color")
     private String color;
+    @Column(name = "company")
     private String company;
+    @Column(name = "category")
     private String category;
 }
